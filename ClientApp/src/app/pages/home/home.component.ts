f7d5609f7786622a549b7f0a077d1b59c8283a25
Bulat-Almukhametov <khattartmasi@rocketmail.com>
2019-06-01 13:38:44 +0300

    Deleted unused template components. Added comments markup.

---
+++ ClientApp/src/app/pages/home/home.component.ts
@@ -0,0 +1,43 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+
+  commentsArray: Note[] = [];
+
+  constructor() {
+
+    let users: User[] = [
+      {nickname: "MickyMouse", admin: false},
+      {nickname: "DonaldDuck", admin: false},
+      {nickname: "WaltDisney", admin: true},
+      {nickname: "Pluto", admin: false},
+    ];
+
+    for (let i = 0; i < 10; i++) {
+      let i = Math.round(Math.random() * (users.length - 1))
+      this.commentsArray.push({
+        user: users[i],
+        title: "Comment " + (i ),
+        body: users[i].nickname + ' says some interestin thing: "So, I think bla bla bla!"',
+        createTime: new Date()
+      });
+    }
+  }
+
+}
+
+interface Note {
+  user: User;
+  title: string;
+  body: string;
+  createTime: Date;
+}
+
+interface User {
+  nickname: string;
+  admin: boolean;
+}
