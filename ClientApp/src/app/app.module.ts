9b9c747a0e8fd240c096838f1bd438c6cc55e0c6
Bulat-Almukhametov <khattartmasi@rocketmail.com>
2019-06-02 11:29:39 +0300

    Added new/edit page.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './pages/home/home.component';
-import { CommentListComponent } from './pages/home/comment-list/comment-list.component';
+import {AppComponent} from './app.component';
+import {NavMenuComponent} from './nav-menu/nav-menu.component';
+import {HomeComponent} from './pages/home/home.component';
+import {CommentListComponent} from './pages/home/comment-list/comment-list.component';
 import {NoteService} from "./common/services/NoteService";
-import { PaginationComponent } from './common/components/pagination/pagination.component';
+import {PaginationComponent} from './common/components/pagination/pagination.component';
+import {EditComponent} from './pages/edit/edit.component';
 
 @NgModule({
   declarations: [
@@ -18,17 +19,21 @@ import { PaginationComponent } from './common/components/pagination/pagination.c
     HomeComponent,
     CommentListComponent,
     PaginationComponent,
+    EditComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'Home/:page', component: HomeComponent}
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: 'List/:page', component: HomeComponent},
+      {path: 'Edit/:id', component: EditComponent},
+      {path: 'New', component: EditComponent}
     ])
   ],
   providers: [NoteService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ ClientApp/src/app/pages/edit/edit.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html'
+})
+export class EditComponent implements OnInit {
+  /**
+   * Text for user describing edit mode.
+   */
+  private editMode: string;
+  private id: number;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.id = Number(this.route.snapshot.params.id);
+    if (this.id) {
+      this.editMode = "Edit";
+      this.loadData(this.id);
+    }
+    else {
+      this.editMode = "New";
+    }
+  }
+
+  loadData (id: number) {
+
+  }
+}
